75d5f6c7e517f60c20879e52dc7960a877fb1e89
Rykerion Studio's <156715563+kasurarykerion@users.noreply.github.com>
2024-11-11 17:48:57 +0100

    Update main.server.lua

--- rojo-project/src/main.server.lua
+++ rojo-project/src/main.server.lua
@@ -1,1 +1,21 @@
+-- main.server.lua
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local function onPlayerJoin(player)
+    print(player.Name .. " has joined the game!")
+    
+    -- Create a object
+    local customObject = Instance.new("Part")
+    customObject.Name = "WelcomePart"
+    customObject.Size = Vector3.new(4, 1, 4)
+    customObject.Position = Vector3.new(0, 10, 0)
+    customObject.Parent = ReplicatedStorage
+    
+    -- Animate the object that we created
+    customObject.Anchored = true
+    customObject.Color = Color3.fromRGB(0, 255, 0)
+end
+
+game.Players.PlayerAdded:Connect(onPlayerJoin)
 
